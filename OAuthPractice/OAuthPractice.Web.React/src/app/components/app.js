958c032e19cc09ed22f144f8c5155fd580549645
unknown <sunf@sunf.cbichina.com>
2015-12-30 10:25:38 +0800

    add react

---
+++ OAuthPractice/OAuthPractice.Web.React/src/app/components/app.js
@@ -0,0 +1,10 @@
+var React=require('react'),
+	ReactDOM=require('react-dom'),
+	BootStrap=require('react-bootstrap'),
+	Panel=BootStrap.Panel,
+	Login=require('./components/login.react');
+ 
+
+
+
+ ReactDOM.render(<Panel />,document.getElementById('app'));
